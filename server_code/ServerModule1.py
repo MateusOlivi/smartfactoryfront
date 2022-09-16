ff8f650ebc0337b2ca7f236594349fb7e428f997
mateusfeitosa.olivi@gmail.com <mateusfeitosa.olivi@gmail.com>
2022-09-16 15:24:24 +0000

    Edited settings, ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -7,11 +7,16 @@ from datetime import datetime
 import random
 #import psycopg2 
 import anvil.http
+import random
   
 @anvil.server.callable
-def get_revenue():
-  return app_tables.revenue.search()
- 
+def get_temp_history():
+    
+    my_history = []
+    for i in range(1,10):
+      my_history.extend([{'datetime':f'2022-09-{i}T{j}:00:00', 'temperature': random.randint(10,30)} for j in range(1, 10)])
+    
+    return my_history
 @anvil.server.callable
 def get_user_signups():
     # Connect and retrieve data from your existing database - example code below
