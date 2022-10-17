b1ebe524b287fd4bb875dcb39f30f4bbd7a37289
mateusfeitosa.olivi@gmail.com <mateusfeitosa.olivi@gmail.com>
2022-10-17 14:58:16 +0000

    Created EditarGrupos.ListaGrupos and EditarGrupos; Edited settings and Login; Deleted Login.RowTemplate1

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -11,7 +11,7 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+        
     # Any code you write here will run when the form opens.
   
   def enter_button(self, **event_args):
