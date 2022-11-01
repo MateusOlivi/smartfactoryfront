8f9ee937e4e770cdfb197b66818badd5feacb780
mateusfeitosa.olivi@gmail.com <mateusfeitosa.olivi@gmail.com>
2022-11-01 18:38:38 +0000

    Created Sensores.EditarSensor; Edited Sensores.SensorTemplate

---
+++ client_code/Sensores/EditarSensor.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import EditarSensorTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class EditarSensor(EditarSensorTemplate):
+  def __init__(self, properties):
+    self.sensor = properties['item']
+    
+    print(self.sensor)
+    # Set Form properties and Data Bindings. 
+    self.init_components(**properties)
+    
+    
+#     self.text_id.text = properties["item"]["id"]
+#     self.text_name.text = properties["item"]["name"]
+#     self.text_limit.text = properties["item"]["limit_value"]
+#     self.text_located_at.text = properties["item"]['located_at']
+#     # Any code you write here will run when the form opens.
+
--- client_code/Sensores/SensorTemplate/__init__.py
+++ client_code/Sensores/SensorTemplate/__init__.py
@@ -10,3 +10,9 @@ class SensorTemplate(SensorTemplateTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def edit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Sensores.EditarSensor', {"item": self.item})
+    
+
