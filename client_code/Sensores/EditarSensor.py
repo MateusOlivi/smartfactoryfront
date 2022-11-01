64fd2a69115d94086b9f1f90a651d217359c15eb
mateusfeitosa.olivi@gmail.com <mateusfeitosa.olivi@gmail.com>
2022-11-01 21:29:07 +0000

    Created Sensores.TypeTemplate; Edited Sensores.EditarSensor

--- client_code/Sensores/EditarSensor.py
+++ client_code/Sensores/EditarSensor.py
@@ -9,6 +9,7 @@ class EditarSensor(EditarSensorTemplate):
     self.sensor = properties['item']
     
     print(self.sensor)
+    self.repeating_panel_1.items = properties['item']['info']
     # Set Form properties and Data Bindings. 
     self.init_components(**properties)
     
