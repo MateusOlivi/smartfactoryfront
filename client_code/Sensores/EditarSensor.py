78dd0440c75bf962aeef2db3468957887bc5bc07
mateusfeitosa.olivi@gmail.com <mateusfeitosa.olivi@gmail.com>
2022-11-02 17:00:44 +0000

    Edited Sensores.TypeTemplate and Sensores.EditarSensor

--- client_code/Sensores/EditarSensor.py
+++ client_code/Sensores/EditarSensor.py
@@ -8,7 +8,6 @@ class EditarSensor(EditarSensorTemplate):
   def __init__(self, properties):
     self.sensor = properties['item']
     
-    print(self.sensor)
     self.repeating_panel_1.items = properties['item']['info']
     # Set Form properties and Data Bindings. 
     self.init_components(**properties)
@@ -19,4 +18,28 @@ class EditarSensor(EditarSensorTemplate):
 #     self.text_limit.text = properties["item"]["limit_value"]
 #     self.text_located_at.text = properties["item"]['located_at']
 #     # Any code you write here will run when the form opens.
+
+  def apply_button_click(self, **event_args):
+    sensor_id = self.sensor["sensor_id"]
+    located_at = self.text_located_at.text
+    
+    if(located_at == ""):
+      located_at = None
+    
+    patch_json = {
+      "located_at": located_at
+    }
+    
+    for item in self.repeating_panel_1.items:
+      if("info" not in patch_json.keys()):
+        patch_json["info"] = []
+        
+      patch_json["info"].append({
+        "type":item["type"],
+        "upper_limit": item["upper_limit"],
+        "bottom_limit": item["bottom_limit"]
+      })
+    
+    print(patch_json)
+
 
